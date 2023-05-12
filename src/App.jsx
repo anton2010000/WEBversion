cc316ef7d1b036dfb70a26d088033f48edcb25ab
Arsalan Rabdanov <uzarsalan@gmail.com>
2023-05-13 02:44:33 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import ProductsList from "./components/ProductsList";
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    const index = cart.findIndex((item) => item.product.id == product.id);
+    if (index >= 0) {
+      cart[index].count++;
+      setCart([...cart]);
+    } else setCart([...cart, { count: 1, product }]);
+  };
+
+  useEffect(() => {
+    if (cart.length) {
+      window.Telegram?.WebApp.MainButton.setText(
+        `В корзине: ${(cart.reduce((sum, item) => (sum += item.count)), 0)}`
+      );
+      if (!window.Telegram?.WebApp.MainButton.isVisible)
+        window.Telegram?.WebApp.MainButton.show();
+    } else {
+      if (window.Telegram?.WebApp.MainButton.isVisible)
+        window.Telegram?.WebApp.MainButton.hide();
+    }
+  }, [cart]);
+
+  return (
+    <main>
+      <ProductsList addToCart={addToCart} cart={cart} />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/ProductsList/index.jsx
@@ -0,0 +1,133 @@
+import { useState } from "react";
+import { CheckIcon } from "@heroicons/react/24/solid";
+
+const products = [
+  {
+    id: 1,
+    name: "Basic Tee",
+    imageSrc: "uploads/product.jpeg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  {
+    id: 2,
+    name: "Basic Tee",
+    imageSrc: "uploads/product.jpeg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  {
+    id: 3,
+    name: "Basic Tee",
+    imageSrc: "uploads/product.jpeg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  {
+    id: 4,
+    name: "Basic Tee",
+    imageSrc: "uploads/product.jpeg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  {
+    id: 5,
+    name: "Basic Tee",
+    imageSrc: "uploads/product.jpeg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  {
+    id: 6,
+    name: "Basic Tee",
+    imageSrc: "uploads/product.jpeg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  {
+    id: 7,
+    name: "Basic Tee",
+    imageSrc: "uploads/product.jpeg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  {
+    id: 8,
+    name: "Basic Tee",
+    imageSrc: "uploads/product.jpeg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  {
+    id: 9,
+    name: "Basic Tee",
+    imageSrc: "uploads/product.jpeg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  {
+    id: 10,
+    name: "Basic Tee",
+    imageSrc: "uploads/product.jpeg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+];
+
+export default function ProductsList({ cart = [], addToCart = () => {} }) {
+  const onClickAdd = (productId) => {
+    addToCart(products.find((item) => item.id == productId));
+  };
+
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+          Товары
+        </h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {products.map((product, index) => (
+            <div key={product.id} className="group relative">
+              <div className="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none lg:h-80">
+                <img
+                  src={product.imageSrc}
+                  alt={product.imageAlt}
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+              </div>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-gray-700">{product.name}</h3>
+                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                </div>
+
+                <button
+                  type="button"
+                  onClick={() => onClickAdd(product.id)}
+                  className="w-20 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                >
+                  {cart.find((item) => item.product.id == product.id) ? (
+                    <CheckIcon className="h-5 mx-auto" />
+                  ) : (
+                    product.price
+                  )}
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
