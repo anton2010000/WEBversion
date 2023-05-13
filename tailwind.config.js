fdbeb45eacbc96c9c1b7b633dd17297bc89742fc
Arsalan Rabdanov <uzarsalan@gmail.com>
2023-05-13 03:11:43 +0300

    tailwind theme fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-color": "var(--tg-theme-bg-color)",
+        "text-color": "var(--tg-theme-text-color)",
+        "hint-color": "var(--tg-theme-hint-color)",
+        "link-color": "var(--tg-theme-link-color)",
+        "button-color": "var(--tg-theme-button-color)",
+        "button-text-color": "var(--tg-theme-button-text-color)",
+        "secondary-bg-color": "var(--tg-theme-secondary-bg-color)",
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-  ],
-}
-
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
