cfb0ec6481c999aa2dc2e0150b75b58410f23795
Arsalan Rabdanov <uzarsalan@gmail.com>
2023-05-13 03:19:00 +0300

    style fixes

--- src/components/ProductsList/index.jsx
+++ src/components/ProductsList/index.jsx
@@ -91,7 +91,7 @@ export default function ProductsList({ cart = [], addToCart = () => {} }) {
 
   return (
     <div className="">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <div className="mx-auto max-w-2xl px-4 py-6 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-bold tracking-tight text-text-color">
           Товары
         </h2>
