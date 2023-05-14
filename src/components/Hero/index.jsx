3ba5e10c67b04e634e10177102e34d285d4cbecb
Arsalan Rabdanov <uzarsalan@gmail.com>
2023-05-14 04:44:27 +0300

    color fixes

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -17,10 +17,10 @@ const Hero = () => {
       </div>
       <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div className="text-center">
-          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          <h1 className="text-4xl font-bold tracking-tight text-text-color sm:text-6xl">
             Лучший интернет-магазин разных товаров
           </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
+          <p className="mt-6 text-lg leading-8 text-text-color">
             У нас все самое дешевое и качественное с доставкой день в день
             <br />
             Мы вернем вам деньги если вас не устроит качество товара
@@ -28,13 +28,13 @@ const Hero = () => {
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link
               to="/catalog"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="rounded-md bg-button-color px-3.5 py-2.5 text-sm font-semibold text-button-text-color shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Каталог
             </Link>
             <Link
               to="/about"
-              className="text-sm font-semibold leading-6 text-gray-900"
+              className="text-sm font-semibold leading-6 text-link-color"
             >
               О нас <span aria-hidden="true">→</span>
             </Link>
